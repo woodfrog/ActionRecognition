9e8b88221cd650ccc9d7fb3d08d6705bbcbc3c6c
CJC <cjc0722hz@gmail.com>
2017-03-15 11:35:40 -0700

    more comments on tf_rnn_tut

--- rnn_practice/tf_rnn_tut/reader_test.py
+++ rnn_practice/tf_rnn_tut/reader_test.py
@@ -27,42 +27,41 @@ import reader
 
 
 class PtbReaderTest(tf.test.TestCase):
+    def setUp(self):
+        self._string_data = "\n".join(
+            [" hello there i am",
+             " rain as day",
+             " want some cheesy puffs ?"])
 
-  def setUp(self):
-    self._string_data = "\n".join(
-        [" hello there i am",
-         " rain as day",
-         " want some cheesy puffs ?"])
+    def testPtbRawData(self):
+        tmpdir = tf.test.get_temp_dir()
+        for suffix in "train", "valid", "test":
+            filename = os.path.join(tmpdir, "ptb.%s.txt" % suffix)
+            with tf.gfile.GFile(filename, "w") as fh:
+                fh.write(self._string_data)
+        # Smoke test
+        output = reader.ptb_raw_data(tmpdir)
+        self.assertEqual(len(output), 4)
 
-  def testPtbRawData(self):
-    tmpdir = tf.test.get_temp_dir()
-    for suffix in "train", "valid", "test":
-      filename = os.path.join(tmpdir, "ptb.%s.txt" % suffix)
-      with tf.gfile.GFile(filename, "w") as fh:
-        fh.write(self._string_data)
-    # Smoke test
-    output = reader.ptb_raw_data(tmpdir)
-    self.assertEqual(len(output), 4)
-
-  def testPtbProducer(self):
-    raw_data = [4, 3, 2, 1, 0, 5, 6, 1, 1, 1, 1, 0, 3, 4, 1]
-    batch_size = 3
-    num_steps = 2
-    x, y = reader.ptb_producer(raw_data, batch_size, num_steps)
-    with self.test_session() as session:
-      coord = tf.train.Coordinator()
-      tf.train.start_queue_runners(session, coord=coord)
-      try:
-        xval, yval = session.run([x, y])
-        self.assertAllEqual(xval, [[4, 3], [5, 6], [1, 0]])
-        self.assertAllEqual(yval, [[3, 2], [6, 1], [0, 3]])
-        xval, yval = session.run([x, y])
-        self.assertAllEqual(xval, [[2, 1], [1, 1], [3, 4]])
-        self.assertAllEqual(yval, [[1, 0], [1, 1], [4, 1]])
-      finally:
-        coord.request_stop()
-        coord.join()
+    def testPtbProducer(self):
+        raw_data = [4, 3, 2, 1, 0, 5, 6, 1, 1, 1, 1, 0, 3, 4, 1]
+        batch_size = 3
+        num_steps = 2
+        x, y = reader.ptb_producer(raw_data, batch_size, num_steps)
+        with self.test_session() as session:
+            coord = tf.train.Coordinator()
+            tf.train.start_queue_runners(session, coord=coord)
+            try:
+                xval, yval = session.run([x, y])
+                self.assertAllEqual(xval, [[4, 3], [5, 6], [1, 0]])
+                self.assertAllEqual(yval, [[3, 2], [6, 1], [0, 3]])
+                xval, yval = session.run([x, y])
+                self.assertAllEqual(xval, [[2, 1], [1, 1], [3, 4]])
+                self.assertAllEqual(yval, [[1, 0], [1, 1], [4, 1]])
+            finally:
+                coord.request_stop()
+                coord.join()
 
 
 if __name__ == "__main__":
-  tf.test.main()
+    tf.test.main()
