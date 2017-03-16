b9f5ec201810f84dba8ad856648a2be0db00cb8e
changan <q604815016@gmail.com>
2017-03-16 10:31:54 -0700

    LRCN v0.1

--- rnn_practice/LRCN_keras.py
+++ rnn_practice/LRCN_keras.py
@@ -1,68 +1,47 @@
+'''
+Implementing simple LRCN structure in paper https://arxiv.org/pdf/1411.4389v3.pdf
+'''
+
+
 import numpy as np
 from keras.models import Sequential, Model
 from keras.layers import Dense, Flatten, Dropout, Reshape, Permute, Activation
 from keras.layers import Convolution2D, MaxPooling3D, ConvLSTM2D
 from keras.layers.recurrent import LSTM
-#import inception_v3 as inception
+import keras.callbacks
 import os, random, cv2
 
 
 N_CLASSES = 101
 IMSIZE = (216, 216)
 SequenceLength = 10
-BatchSize = 1
+BatchSize = 2
 
 def load_model():
-    '''
-    # Start with an Inception V3 model, not including the final softmax layer.
-    base_model = inception.InceptionV3(weights='imagenet', input_tensor=())
-    print('Loaded Inception model')
-
-    # Turn off training on base model layers
-    for layer in base_model.layers:
-        layer.trainable = False
-
-    # Add on new fully connected layers for the output classes.
-    x = LSTM(32, name='lstm1', return_sequences=True)(base_model.get_layer('flatten').output)
-    x = LSTM(32, name='lstm2', return_sequences=False)(x)
-    predictions = Dense(N_CLASSES, activation='softmax', name='predictions')(x)
-    model = Model(input=base_model.input, output=predictions)
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    '''
     # use simple CNN structure
     in_shape = (SequenceLength, IMSIZE[0], IMSIZE[1], 3)
     model = Sequential()
-    model.add(ConvLSTM2D(2, 3, 3, border_mode='valid', return_sequences=True, input_shape=in_shape))
+    model.add(ConvLSTM2D(32, 7, 7, border_mode='valid', return_sequences=True, input_shape=in_shape))
     model.add(Activation('relu'))
-    # model.add(ConvLSTM2D(2, 3, 3, border_mode='valid', return_sequences=True))
-    # model.add(Activation('relu'))
-    model.add(MaxPooling3D(pool_size=(1, 4, 4)))# channel last
-    model.add(Dropout(0.25))
-
-    # model.add(ConvLSTM2D(2, 3, 3, border_mode='valid', return_sequences=True))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling3D(pool_size=(1, 2, 2)))
-    # model.add(Dropout(0.25))
-    # model.add(ConvLSTM2D(2, 3, 3, border_mode='valid', return_sequences=True))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling3D(pool_size=(1, 2, 2)))
-    # model.add(Dropout(0.25))
-    #
-    # model.add(ConvLSTM2D(2, 3, 3, border_mode='valid', return_sequences=True))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling3D(pool_size=(1, 2, 2)))
-    # model.add(Dropout(0.25))
-    # model.add(ConvLSTM2D(2, 3, 3, border_mode='valid', return_sequences=True))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling3D(pool_size=(1, 2, 2)))
-    # model.add(Dropout(0.25))
+    model.add(MaxPooling3D(pool_size=(1, 2, 2)))# channel last
+    model.add(ConvLSTM2D(64, 5, 5, border_mode='valid', return_sequences=True))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(1, 2, 2)))
+    model.add(ConvLSTM2D(96, 3, 3, border_mode='valid', return_sequences=True))
+    model.add(Activation('relu'))
+    model.add(ConvLSTM2D(96, 3, 3, border_mode='valid', return_sequences=True))
+    model.add(Activation('relu'))
+    model.add(ConvLSTM2D(96, 3, 3, border_mode='valid', return_sequences=True))
+    model.add(MaxPooling3D(pool_size=(1, 2, 2)))
+    model.add(Dense(320))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
 
     out_shape = model.output_shape
-    print('====Model shape: ', out_shape)
-    # model.add(Flatten())
+    # print('====Model shape: ', out_shape)
     model.add(Reshape((SequenceLength, out_shape[2]*out_shape[3]*out_shape[4])))
-    # model.add(LSTM(1, return_sequences=True))
-    model.add(LSTM(1, return_sequences=False))
+    model.add(LSTM(64, return_sequences=False))
+    model.add(Dropout(0.5))
     model.add(Dense(N_CLASSES, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
@@ -75,42 +54,41 @@ def video_generator(data, batch_size):
     x_shape = (batch_size, SequenceLength, IMSIZE[0], IMSIZE[1], 3)
     y_shape = (batch_size, 101)
     index = 0
-    while(True):
+    while True:
         batch_x = np.ndarray(x_shape)
         batch_y = np.zeros(y_shape)
         for i in range(batch_size):
             index = (index + 1) % len(data)
             clip_dir, clip_class = data[index]
             batch_y[i, clip_class-1] = 1
+            clip_dir = os.path.splitext(clip_dir)[0]+'.npy'
             while not os.path.exists(clip_dir):
                 index = (index+1) % len(data)
                 clip_dir, class_idx = data[index]
-            cap = cv2.VideoCapture(clip_dir)
-            j = 0
-            if cap.isOpened():
-                while True:
-                    ret, frame = cap.read()
-                    if ret:
-                        batch_x[i][j] = frame
-                    else:
-                        break
-            cap.release()
+            clip_data = np.load(clip_dir)
+            batch_x[i] = clip_data
         yield batch_x, batch_y
 
 
 def fit_model(model, train_data, test_data):
     try:
+        if os.path.exists('LRCN_keras.h5'):
+            model.load_weights('LRCN_keras.h5')
+            print('Load weights')
         train_generator = video_generator(train_data, BatchSize)
         test_generator = video_generator(test_data, BatchSize)
+        print('Start fitting model')
+        weigts_dir='./weights'
         model.fit_generator(
             train_generator,
-            samples_per_epoch=1,
-            nb_epoch=10,
+            samples_per_epoch=9000,
+            nb_epoch=2000,
             validation_data=test_generator,
-            nb_val_samples=32,
+            nb_val_samples=300,
             verbose=2,
+            callbacks=keras.callbacks.ModelCheckpoint('LRCN_weights.h5', save_weights_only=True)
         )
-        model.save_weights('inception_lstm_weights.h5')
+        model.save_weights('LRCN_keras.h5')
     except KeyboardInterrupt:
         print('Training time:')
 
@@ -118,22 +96,20 @@ def fit_model(model, train_data, test_data):
 def get_data_list(list_dir, video_dir):
     train_dir = os.path.join(video_dir, 'train')
     test_dir = os.path.join(video_dir, 'test')
-    testlisttxt = ['testlist01.txt', 'testlist02.txt', 'testlist03.txt']
-    trainlisttxt = ['trainlist01.txt', 'trainlist02.txt', 'trainlist03.txt']
+    testlisttxt = 'testlist01.txt'
+    trainlisttxt = 'trainlist01.txt'
 
     testlist = []
-    for fname in testlisttxt:
-        txt_path = os.path.join(list_dir, fname)
-        with open(txt_path) as fo:
-            for line in fo:
-                testlist.append(line)
+    txt_path = os.path.join(list_dir, testlisttxt)
+    with open(txt_path) as fo:
+        for line in fo:
+            testlist.append(line.rstrip())
 
     trainlist = []
-    for fname in trainlisttxt:
-        txt_path = os.path.join(list_dir, fname)
-        with open(txt_path) as fo:
-            for line in fo:
-                trainlist.append(line[:line.rfind(' ')])
+    txt_path = os.path.join(list_dir, trainlisttxt)
+    with open(txt_path) as fo:
+        for line in fo:
+            trainlist.append(line[:line.rfind(' ')])
 
     class_index = dict()
     class_dir = os.path.join(list_dir, 'classInd.txt')
@@ -172,16 +148,3 @@ if __name__ == '__main__':
     model = load_model()
     fit_model(model, train_data, test_data)
 
-
-
-'''
-img_path = '/home/changan/419/sport3/validation/hockey/img_2997.jpg'
-img = image.load_img(img_path, target_size=IMSIZE)
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-
-x = inception.preprocess_input(x)
-
-preds = model.predict(x)
-print('Predicted:', preds)
-'''
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,43 +1,40 @@
 import numpy as np
+import scipy.misc
 import os, cv2, random
 
+SequenceLength = 10
+IMSIZE = (216, 216, 3)
+
+
 def combine_list_txt(list_dir):
-    testlisttxt = ['testlist01.txt', 'testlist02.txt', 'testlist03.txt']
-    trainlisttxt = ['trainlist01.txt', 'trainlist02.txt', 'trainlist03.txt']
+    testlisttxt = 'testlist01.txt'
+    trainlisttxt = 'trainlist01.txt'
 
     testlist = []
-    for fname in testlisttxt:
-        txt_path = os.path.join(list_dir, fname)
-        with open(txt_path) as fo:
-            for line in fo:
-                testlist.append(line.rstrip())
+    txt_path = os.path.join(list_dir, testlisttxt)
+    with open(txt_path) as fo:
+        for line in fo:
+            testlist.append(line.rstrip())
 
     trainlist = []
-    for fname in trainlisttxt:
-        txt_path = os.path.join(list_dir, fname)
-        with open(txt_path) as fo:
-            for line in fo:
-                trainlist.append(line[:line.rfind(' ')])
+    txt_path = os.path.join(list_dir, trainlisttxt)
+    with open(txt_path) as fo:
+        for line in fo:
+            trainlist.append(line[:line.rfind(' ')])
 
     return trainlist, testlist
 
-# extract video clip to 5 FPS, down sample resolution to 216*216, and make sequence length of 10
-def process_clip(src_dir, dst_dir):
-    SequenceLength = 10
-    IMSIZE = (216, 216)
-
-    cap = cv2.VideoCapture(src_dir)
+# down sample image resolution to 216*216, and make sequence length 10
+def process_clip(src_dir, dst_dir, mean):
     all_frames = []
-    if (cap.isOpened()):
-        while (True):
-            ret, frame = cap.read()
-            if ret:
-                all_frames.append(frame)
-            else:
-                break
-    else:
-        print('Opening ', src_dir, ' fails')
-        return
+    cap = cv2.VideoCapture(src_dir)
+    while cap.isOpened():
+        succ, frame = cap.read()
+        if not succ:
+            break
+        # append frame that is not all zeros
+        if frame.any():
+            all_frames.append(frame)
 
     clip_length = len(all_frames)
     if clip_length <= 20:
@@ -47,21 +44,26 @@ def process_clip(src_dir, dst_dir):
     index = random.randrange(step_size)  # generate random starting index
     for i in range(SequenceLength):
         frame = all_frames[index]
-        frame = cv2.resize(frame, IMSIZE)
-        # cv2.imshow('f', frame)
-        # cv2.waitKey(30)
+        frame = scipy.misc.imresize(frame, IMSIZE)
+        frame = frame.astype(dtype='int16')
+        frame -= mean
         frame_sequence.append(frame)
         index += step_size
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(dst_dir, fourcc, 5.0, IMSIZE)
-    for frame in frame_sequence:
-        out.write(frame)
+    frame_sequence = np.stack(frame_sequence, axis=0)
+    dst_dir = os.path.splitext(dst_dir)[0]+'.npy'
+    np.save(dst_dir, frame_sequence)
 
-    out.release()
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # out = cv2.VideoWriter(dst_dir, fourcc, 5.0, IMSIZE)
+    # for frame in frame_sequence:
+    #     out.write(frame)
+    # out.release()
+
     cap.release()
 
 def Preprocessing(list_dir, UCF_dir, dest_dir):
     if os.path.exists(dest_dir):
+        print('Destination directory already exists')
         return
     os.mkdir(dest_dir)
     trainlist, testlist = combine_list_txt(list_dir)
@@ -69,6 +71,8 @@ def Preprocessing(list_dir, UCF_dir, dest_dir):
     test_dir = os.path.join(dest_dir, 'test')
     os.mkdir(train_dir)
     os.mkdir(test_dir)
+    mean = calc_mean(UCF_dir).astype(dtype='int16')
+    np.save(os.path.join(dest_dir, 'mean.npy'), mean)
 
     print('Processing train data')
     for clip in trainlist:
@@ -76,11 +80,11 @@ def Preprocessing(list_dir, UCF_dir, dest_dir):
         clip_category = os.path.dirname(clip)
         category_dir = os.path.join(train_dir, clip_category)
         src_dir = os.path.join(UCF_dir, clip)
-        print(src_dir)
+        # print(src_dir)
         dst_dir = os.path.join(category_dir, clip_name)
         if not os.path.exists(category_dir):
             os.mkdir(category_dir)
-        process_clip(src_dir, dst_dir)
+        process_clip(src_dir, dst_dir, mean)
 
     print('Processing test data')
     for clip in testlist:
@@ -88,18 +92,41 @@ def Preprocessing(list_dir, UCF_dir, dest_dir):
         clip_category = os.path.dirname(clip)
         category_dir = os.path.join(test_dir, clip_category)
         src_dir = os.path.join(UCF_dir, clip)
-        print(src_dir)
+        # print(src_dir)
         dst_dir = os.path.join(category_dir, clip_name)
         if not os.path.exists(category_dir):
             os.mkdir(category_dir)
-        process_clip(src_dir, dst_dir)
+        process_clip(src_dir, dst_dir, mean)
 
+
+def calc_mean(UCF_dir):
+    frames = []
+    print('Calculating RGB mean ...')
+    for dirpath, dirnames, filenames in os.walk(UCF_dir):
+        for filename in filenames:
+            path = os.path.join(dirpath, filename)
+            if os.path.exists(path):
+                cap = cv2.VideoCapture(path)
+                if cap.isOpened():
+                    ret, frame = cap.read()
+                    # successful read and frame should not be all zeros
+                    if ret and frame.any():
+                        if frame.shape != (240, 320, 3):
+                            frame = scipy.misc.imresize(frame, (240, 320, 3))
+                        frames.append(frame)
+                cap.release()
+    frames = np.stack(frames)
+    mean = frames.mean(axis=0, dtype='int64')
+    mean = scipy.misc.imresize(mean, IMSIZE)
+    print('RGB mean is calculated over', len(frames), 'video frames')
+    return mean
 
 
 if __name__ == '__main__':
     data_dir = '/home/changan/ActionRocognition_rnn/data'
     list_dir = os.path.join(data_dir, 'ucfTrainTestlist')
     UCF_dir = os.path.join(data_dir, 'UCF-101')
-    dest_dir = os.path.join(data_dir, 'UCF-Preprocessed')
+    dest_dir = os.path.join(data_dir, 'UCF-Preprocessed1')
 
+    # calc_mean(UCF_dir)
     Preprocessing(list_dir, UCF_dir, dest_dir)
